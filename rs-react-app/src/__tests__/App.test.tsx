4400adf8d980326b40301f341c7a794775a3af12
Muhammadshoh01 <rajabovmuhammadshoh009@gmail.com>
2025-07-20 17:28:58 +0500

    refactor: changes in App test

--- rs-react-app/src/__tests__/App.test.tsx
+++ rs-react-app/src/__tests__/App.test.tsx
@@ -1,50 +1,40 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from '../App';
+import * as api from '../api';
 import { describe, it, expect, vi, beforeEach } from 'vitest';
+import type { Pokemon } from '../types';
+
+vi.mock('../api.ts');
+
+const mockPokemon: Pokemon = {
+    name: 'bulbasaur',
+    base_experience: 64,
+    sprites: {
+        front_default:
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+    },
+};
+
 
 describe('App Component', () => {
     beforeEach(() => {
+        vi.resetAllMocks();
         localStorage.clear();
     });
+    const mockFetchPokemonList = api.fetchPokemonList as unknown as ReturnType<typeof vi.fn>;
+    const mockFetchPokemonByName = api.fetchPokemonByName as unknown as ReturnType<typeof vi.fn>;
 
     it('renders initial UI correctly', () => {
+        mockFetchPokemonList.mockResolvedValueOnce({
+            results: [{ name: 'bulbasaur', url: '...' }],
+        });
+        mockFetchPokemonByName.mockResolvedValueOnce(mockPokemon);
         render(<App />);
 
         expect(screen.getByTestId('header')).toBeInTheDocument();
         expect(screen.getByTestId('search-input')).toBeInTheDocument();
-        screen.debug()
         waitFor(() => {
             expect(screen.getByTestId('card-list')).toHaveTextContent('Search term:');
         })
-    });
-
-    it('sets searchTerm from localStorage', () => {
-        localStorage.setItem('searchTerm', 'pikachu');
-        render(<App />);
-        waitFor(() => {
-            expect(screen.getByTestId('card-list')).toHaveTextContent('pikachu');
-        })
-    });
-
-    // it('updates search term when typed in search bar', () => {
-    //     render(<App />);
-    //     const input = screen.getByTestId('search-input');
-    //     fireEvent.change(input, { target: { value: 'charmander' } });
-
-    //     // The component re-renders instantly
-    //     expect(screen.getByTestId('card-list')).toHaveTextContent('charmander');
-    // });
-
-    it('throws error when "Throw Error" button is clicked', () => {
-        // Use a try-catch block to catch the error and test the throw
-        const consoleError = vi.spyOn(console, 'error').mockImplementation(() => { }); // silence React error logs
-
-        expect(() => {
-            render(<App />);
-            const throwBtn = screen.getByText(/throw error/i);
-            fireEvent.click(throwBtn);
-        }).toThrow('Manually thrown error in render');
-
-        consoleError.mockRestore();
     });
 });
